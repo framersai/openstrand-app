53949796699378e4dcc0161e8e832d62a99d9fbd
Johnny Dunn <johnnyfived@protonmail.com>
2025-11-07 17:37:18 -0800

    fix: remove Babel config to use SWC compiler

--- next.config.js
+++ next.config.js
@@ -4,8 +4,6 @@ const withNextIntl = require('next-intl/plugin')('./src/i18n/request.ts');
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  // Disable SWC compiler due to parser bug in Next.js 14.2.33
-  // Use Babel instead for more reliable JSX parsing
   compiler: {
     removeConsole: process.env.NODE_ENV === 'production',
   },
@@ -55,16 +53,6 @@ const nextConfig = {
         permanent: false,
       },
     ];
-  },
-  
-  // Use Babel instead of SWC for compilation
-  experimental: {
-    forceSwcTransforms: false,
-  },
-  
-  // Babel config
-  babel: {
-    presets: ['next/babel'],
   },
 };
 
